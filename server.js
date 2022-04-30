16b048b7b8b1e1af4d1ba404ccc7f37f2f9709d2
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-30 17:40:58 -0400

    server running

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, (err) => {
+    !err ? console.log(`App running at http://localhost:${PORT}`) : console.log(err);
+})
