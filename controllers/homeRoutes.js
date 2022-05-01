7876ee756c018d13d5a483137b9a77f62e9bb700
Robert Slocumbe <rslocumbe93@gmail.com>
2022-05-01 17:45:13 -0400

    sql data returning from get requst

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
+const { User, Post, Comment } = require("../models");
 
-router.get("/", (req, res) => {
-    res.send("This route works");
+router.get("/", async (req, res) => {
+    const posts = await Post.findAll({ include: Comment });
+
+    res.status(200).json(posts);
 });
 
 
